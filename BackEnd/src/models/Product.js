28d1ff974cc24badec408b89d655630bd5aa6b5b
Vitor Levi <levivitor8@gmail.com>
2025-06-29 13:57:10 -0300

    feat: Feito o Model do Produto

---
+++ BackEnd/src/models/Product.js
@@ -0,0 +1,28 @@
+// src/models/Product.js
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    pricePerKg: {
+        type: Number,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
