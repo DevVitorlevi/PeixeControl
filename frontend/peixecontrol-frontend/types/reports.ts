8094be76ee11a61bdeb1443d06701b368766f44f
Vitor Levi <levivitor8@gmail.com>
2026-08-08 15:46:17 -0300

    feat(reports): add TopProduct type

--- frontend/peixecontrol-frontend/types/reports.ts
+++ frontend/peixecontrol-frontend/types/reports.ts
@@ -2,3 +2,10 @@ export interface SalesSummary {
   totalSalesValue: number;
   totalQuantity: number;
 }
+
+export interface TopProduct {
+  _id: string;
+  productName: string;
+  totalQuantity: number;
+  totalSalesValue: number;
+}
